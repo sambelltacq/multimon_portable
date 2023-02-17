c478d3f4f1a7eb732e4364829ea6d1e1322211a9
sambelltacq <Sam.Bell@d-tacq.co.uk>
2023-02-17 14:14:21 +0000

    Initial Commit 	new file:   config.json 	new file:   multimon.py sam@humla

---
+++ multimon.py
@@ -0,0 +1,425 @@
+#!/usr/bin/env python3
+
+import sqlite3
+import socket
+import time
+import re
+import json
+import os
+import requests
+import xmltodict
+import epics
+import threading
+
+from flask import Flask
+
+class globals:
+	db = None
+
+	lock = None
+
+	lighthouses = ['acq1001_074']
+
+	lighthouse = None
+
+	tty_servers = []
+
+	active_ttys = {}
+
+	web_port = 88
+
+	update_rate = 1
+
+	active_uuts = set()
+
+	table_name = 'states'
+
+	primary_key = 'uut_name'
+
+	table_schema = {
+		'uut_name' 	: 'TEXT PRIMARY KEY',
+		'ip'		: 'TEXT',
+		'tty'		: 'TEXT',
+		'user'		: 'TEXT',
+		'test'		: 'TEXT',
+		'delay'		: 'INTEGER',
+		'conn_type'	: 'TEXT',
+		#do not change above
+		'uptime'	: 'INTEGER',
+		'temp'		: 'REAL',
+		'firmware'	: 'TEXT',
+		'fpga'		: 'TEXT',
+		'cstate'	: 'INTEGER',
+		'tstate'	: 'INTEGER',
+		'shot'		: 'INTEGER',
+		'clk_set'	: 'REAL',
+		'clk_freq'	: 'REAL',
+	}
+	mapped_knobs = {
+		'host'					: 'uut_name',
+		'SYS:UPTIME'			: 'uptime',
+		'SYS:0:TEMP'			: 'temp',
+		'SYS:VERSION:SW'		: 'firmware',
+		'SYS:VERSION:FPGA'		: 'fpga',
+		'MODE:CONTINUOUS:STATE' : 'cstate',
+		'MODE:TRANS_ACT:STATE' 	: 'tstate',
+		'1:SHOT'				: 'shot',
+		'0:SIG:CLK_MB:SET'		: 'clk_set',
+		'0:SIG:CLK_MB:FREQ'		: 'clk_freq',
+	}
+
+class Uut_connector:
+	def __init__(self, hostname):
+		self.hostname = hostname
+		self.db = globals.db
+		self.state = {}
+		self.state[globals.primary_key] = hostname
+		self.offline = 0
+		self.last_contact = 0
+
+		self.epic_last = 0
+		self.epics_callbacks = []
+		self.check_legacy()
+		self.create_record()
+
+	def check_legacy(self):
+		self.url = f'http://{self.hostname}/d-tacq/data/status.xml'
+		try:
+			response = requests.head(self.url)
+			self.state['ip'] = socket.gethostbyname(self.hostname)
+		except Exception as e:
+			print(e)
+			self.__connection_down()
+			return
+		if response.status_code != 200:
+			self.legacy = True
+			self.state['conn_type'] = 'EPICS'
+			self.get_state = self.__get_status_epics
+			return
+		self.legacy = False
+		self.state['conn_type'] = 'WEB'
+		self.get_state = self.__get_status_http
+
+	def __get_status_http(self):
+		try:
+			response = requests.get(self.url, timeout=2.50)
+		except Exception as e:
+			print(e)
+			self.__connection_down()
+			return
+		if response.status_code != 200:
+			self.__connection_down()
+			return
+		data = xmltodict.parse(response.content)
+		self.state.update(self.__data_extractor(data))
+		self.__connection_up()
+
+	def __data_extractor(self, data):
+		extracted_data = {}
+		if type(data) is dict:
+			if '@n' in data:
+				if data['@n'] in globals.mapped_knobs:
+					extracted_data[globals.mapped_knobs[data['@n']]] = data['v']
+				return extracted_data
+			for key, value in data.items():
+				if type(value) is not str:
+					extracted_data.update(self.__data_extractor(value))
+					continue
+				if key in globals.mapped_knobs:
+					extracted_data[globals.mapped_knobs[key]] = value
+			return extracted_data
+		if type(data) is list:
+			for item in data:
+				extracted_data.update(self.__data_extractor(item))
+		return extracted_data
+
+	def __get_status_epics(self):
+		if not self.epic_last:
+			if self.is_epics_down():
+				self.kill_self()
+			knobs = globals.mapped_knobs.copy()
+			del knobs['host']
+			for knob in knobs:
+				knob = f'{self.hostname}:{knob}'
+				self.epics_callbacks.append(epics.PV(knob, auto_monitor=True, form='native', callback=self.__epics_callback))
+			self.epic_last = time.time()
+			return
+		if time.time() - self.epic_last > 60:
+			for callback in self.epics_callbacks:
+				callback.disconnect()
+			self.epic_last = 0
+			self.epics_callbacks = []
+		self.offline += 1
+
+
+	def __epics_callback(self, **kws):
+		host = f'{self.hostname}:'
+		key = kws['pvname'].replace(host,'')
+		value = kws['value']
+		self.state[globals.mapped_knobs[key]] = value
+		self.offline = 0
+
+	def check_tty(self):
+		if self.hostname in globals.active_ttys:
+			self.state['tty'] = globals.active_ttys[self.hostname]
+
+	def create_record(self):
+		keys = ''
+		values = ''
+		for key, value in self.state.items():
+			keys += f'{key},'
+			values += f'"{value}",'
+		sql = f'INSERT OR IGNORE INTO {globals.table_name} ({keys[:-1]}) VALUES ({values[:-1]})'
+		self.__run_query(sql)
+
+	def update_record(self):
+		self.state['delay'] = int(self.offline)
+		changes = ''
+		for key, value in self.state.items():
+			if key == globals.primary_key:
+				continue
+			changes += f'{key} = "{value}",'
+		sql = f'UPDATE {globals.table_name} SET {changes[:-1]} where {globals.primary_key} = "{self.state[globals.primary_key]}"'
+		self.__run_query(sql)
+
+	def delete_record(self):
+		sql = f'DELETE FROM {globals.table_name} WHERE {globals.primary_key} = "{self.state[globals.primary_key]}"'
+		self.__run_query(sql)
+
+	def __run_query(self, sql):
+		cursor = self.db.cursor()
+		try:
+			cursor.execute(sql)
+		except Exception as e:
+			print(e)
+			self.__connection_down()
+
+	def __connection_down(self):
+		prRed(f"{self.hostname} is DOWN")
+		if not self.offline:
+			self.last_contact = time.time()
+		self.offline = time.time() - self.last_contact
+
+	def __connection_up(self):
+			self.offline = 0
+			self.last_contact = 0
+
+	def is_epics_down(self):
+		try:
+			s = socket.socket()
+			s.settimeout(3)
+			s.connect((self.hostname, 80))
+		except Exception as e:
+			print(e)
+			return True
+		return False
+
+	def is_dead(self):
+		max_wait = 10
+		if self.offline > max_wait:
+			return True
+		return False
+
+	def kill_self(self):
+		globals.active_uuts.remove(self.hostname)
+		self.delete_record()
+		prRed(f'{self.hostname} is offline removing')
+		exit()
+
+######################################################################################
+
+
+
+def main():
+	get_config_file()
+	create_db()
+	find_lighthouse()
+	handler = threading.Thread(target=thread_handler)
+	handler.start()
+	start_web()
+
+def get_config_file():
+	config_file = 'config.json'
+	valid_keys = {'lighthouses','update_rate','web_port','tty_servers'}
+	if os.path.exists(config_file):
+		file = open(config_file, "r")
+		data = json.loads(file.read())
+		for key in valid_keys:
+			if key in data:
+				setattr(globals, key, data[key])
+
+def create_db():
+	db = sqlite3.connect(":memory:", check_same_thread=False)
+	cursor = db.cursor()
+	if not table_exists(cursor, globals.table_name):
+		sql = build_create_sql(globals.table_name, globals.table_schema)
+		cursor.execute(sql)
+	globals.db = db
+
+def build_create_sql(table_name, table_schema):
+	sql = f"CREATE TABLE {table_name} ("
+	for column in table_schema.items():
+		sql += f"{column[0]} {column[1]},"
+	sql = sql[:-1] + ")"
+	return sql
+
+def table_exists(cursor, table_name):
+	sql = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'"
+	tables = cursor.execute(sql).fetchall()
+	if len(tables) == 0:
+		return False
+	return True
+
+def find_lighthouse():
+	for uut in globals.lighthouses:
+		if check_if_casw(uut):
+			prBlue(f"casw server established {uut}")
+			globals.lighthouse = uut
+			return
+	exit(prRed("No Valid casw systems found"))
+
+def check_if_casw(host):
+	s = socket.socket()
+	s.settimeout(3)
+	try:
+		s.connect((host, 54555))
+		data = s.recv(1024)
+	except:
+		return False
+	else:
+		if not data:
+			return False
+		s.close()
+		return True
+
+def thread_handler():
+	tty_monitor = threading.Thread(target=get_tty_connections)
+	tty_monitor.start()
+	s = connect_to_lighthouse()
+	expr = re.compile('([\w]+)[\.\w\-]*:5064')
+	uut_threads = {}
+	last_dead_check = time.time()
+	while True:
+		matches = get_lighthouse_data(s)
+		if not matches:
+			s = connect_to_lighthouse()
+			continue
+		for hostname in matches:
+			if hostname in globals.active_uuts:
+				continue
+			if hostname.isnumeric():
+				continue
+			prGreen(f'Adding {hostname}')
+			globals.active_uuts.add(hostname)
+			uut_object = Uut_connector(hostname)
+			clip_thread = threading.Thread(target=clipper, args=(uut_object,))
+			clip_thread.start()
+			uut_threads[hostname] = clip_thread
+		prYellow(f'{len(globals.active_uuts)} uuts active')
+		time.sleep(1)
+		if time.time() - last_dead_check > 60:
+			prRed('Checking dead threads')
+			last_dead_check = time.time()
+			for hostname in uut_threads.copy().keys():
+				if not uut_threads[hostname].is_alive():
+					if hostname in globals.active_uuts:
+						globals.active_uuts.remove(hostname)
+					del uut_threads[hostname]
+					prRed(f'{hostname} thread has died unexpectedly')
+
+def connect_to_lighthouse():
+	s = socket.socket()
+	try:
+		s.connect((globals.lighthouse, 54555))
+	except:
+		find_lighthouse()
+		s.connect((globals.lighthouse, 54555))
+	return s
+
+def get_lighthouse_data(socket):
+	expr = re.compile('([\w]+)[\.\w\-]*:5064')
+	data = socket.recv(1024).decode().strip()
+	matches = expr.findall(data)
+	return matches
+
+def get_tty_connections():
+	while True:
+		for tty in globals.tty_servers:
+			url = f'http://{tty}/cgi-bin/showconsoles.cgi'
+			try:
+				headers = {'Accept-Encoding': 'identity'}
+				response = requests.get(url, headers=headers)
+			except Exception as e:
+				continue
+			if response.status_code != 200:
+				continue
+			for line in response.content.decode().split('\n'):
+				match = re.match('tty_([a-zA-Z0-9_]+)', line)
+				if match:
+					hostname = match[1]
+					globals.active_ttys[hostname] = tty
+		time.sleep(60)
+
+def clipper(uut_object):
+	while True:
+		uut_object.get_state()
+		uut_object.check_tty()
+		uut_object.update_record()
+		time.sleep(globals.update_rate)
+		if uut_object.is_dead():
+			uut_object.kill_self()
+
+
+def start_web():
+	app = Flask(__name__)
+
+	@app.route("/")
+	def root():
+		return "hello world"
+	def hello_world():
+		cursor = globals.db.cursor()
+		sql = f"SELECT * FROM states ORDER BY uut_name;"
+		rows = cursor.execute(sql).fetchall()
+		total = len(rows)
+		header = '<tr>'
+		for key in globals.table_schema.keys():
+			header += f'<th>{key}</th>'
+		header += '</tr>'
+		content = ''
+		for row in rows:
+			content += '<tr>'
+			for value in row:
+				content += f'<td>{value}</td>'
+			content += '</tr>'
+		return f"""
+		<h1>MULTIMON PORTABLE</h1>
+			<table>
+				{header}
+				{content}
+			</table>
+			<h3>Total: {total}</h3>
+
+
+			"""
+
+	@app.route("/state")
+	def web_update():
+		return "<p>state.json</p>"
+
+	@app.route("/endpoint")
+	def web_set():
+		return "<p>set test set users</p>"
+
+	app.run(host="0.0.0.0", port=globals.web_port)
+
+def prRed(skk): print("\033[91m{}\033[00m" .format(skk))
+def prGreen(skk): print("\033[92m{}\033[00m" .format(skk))
+def prYellow(skk): print("\033[93m{}\033[00m" .format(skk))
+def prPurple(skk): print("\033[95m{}\033[00m" .format(skk))
+def prCyan(skk): print("\033[96m{}\033[00m" .format(skk))
+def prBlue(skk): print("\033[94m{}\033[00m" .format(skk))
+
+
+if __name__ == '__main__':
+	main()
+
